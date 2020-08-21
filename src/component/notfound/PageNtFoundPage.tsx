48575f9b03520180a210f690c9f5e94882c76466
Pascal <duschen.pascal@gmail.com>
2020-08-21 15:39:26 +0200

    added pagenotfound and about component

---
+++ src/component/notfound/PageNtFoundPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageNtFoundPage: React.FC = () => {
+    return (
+        <div>
+            <h1>PageNotFound</h1>
+        </div>
+    )
+}
+
+export default PageNtFoundPage
