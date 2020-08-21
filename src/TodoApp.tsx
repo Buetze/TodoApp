0a6548d40329fc204f758de282b40e559c87c671
Pascal <duschen.pascal@gmail.com>
2020-08-21 10:48:36 +0200

    initial commit

---
+++ src/TodoApp.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TodoApp() {
+  return (
+    <React.Fragment>
+      <h1>
+        todo Application
+      </h1>
+    </React.Fragment>
+  );
+}
+
+export default TodoApp;
